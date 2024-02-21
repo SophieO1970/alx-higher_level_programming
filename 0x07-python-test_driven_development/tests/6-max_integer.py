82af0edf6fb5c9ebc3684d0b2b061cca1485634a
SophieO1970 <sophieolondie@gmail.com>
2024-02-21 11:25:39 -0800

    corrections on the tests files,create the python files, excute the tests file and py file

--- 0x07-python-test_driven_development/tests/6-max_integer.py
+++ 0x07-python-test_driven_development/tests/6-max_integer.py
@@ -2,17 +2,19 @@
 """Module to find the max integer in a list
 """
 
-
-def max_integer(list=[]):
+def max_integer(lst=[]):
     """Function to find and return the max integer in a list of integers
     If the list is empty, the function returns None
     """
-if len(list) == 0:
-    return None
-result = list[0]
-i = 1
-while i < len(list):
-    if list[i] > result:
-        result = list[i]
+    if len(lst) == 0:
+        return None
+    
+    result = lst[0]
+    i = 1
+
+    while i < len(lst):
+        if lst[i] > result:
+            result = lst[i]
         i += 1
+    
     return result
